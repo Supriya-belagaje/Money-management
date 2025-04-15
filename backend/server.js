114177f9768d5072d5ada7bfb0ae866657de73a6
Supriya-belagaje <supriyabelagaje@gmail.com>
2025-04-15 14:42:45 +0530

    Resolved merge conflicts in login and updated backend files

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,10 @@ app.use(express.json());
 connectDB();
 
 // Routes
+app.use("/api/users", require("./routes/login"));
 app.use("/api/users", require("./routes/reg"));
 app.use("/api/transactions", require("./routes/transactionRoutes"));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
