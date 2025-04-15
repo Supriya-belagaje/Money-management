3bfdd74b5cdc8a35ae7d35a46c0318aaed9249c0
General-kp <colonelkpunit3112001@gmail.com>
2025-04-15 12:03:05 +0530

    error middleware

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,9 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_URI,{
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+      useUnifiedTopology: true});
     console.log("MongoDB Connected ✅");
   } catch (error) {
     console.error("MongoDB Connection Error ❌", error);
